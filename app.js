7e74aea49cf8d9bb973d037bd251a44a22cfcc46
youssef <youssefelouarrat1995@gmail.com>
2020-11-26 21:15:19 +0100

    passport jwt

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session =require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
+var config = require('./config');
 
 
 var indexRouter = require('./routes/index');
@@ -20,7 +20,7 @@ const mongoose =require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -38,35 +38,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 /// takes a secret key aleatoire : 12345-12345-0289
 // app.use(cookieParser('12345-12345-0289'));
-app.use(session({
-      name :'session-id',
-      secret : '12345-12345-0289',
-      saveUninitialized  : false,
-      resave : false,
-      store :new FileStore()
-}));
+// app.use(session({
+//       name :'session-id',
+//       secret : '12345-12345-0289',
+//       saveUninitialized  : false,
+//       resave : false,
+//       store :new FileStore()
+// }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 ////////////////////////////////////////////////::
 // . . .
-function auth (req, res, next) {
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
-    else {
-        next();
-    }
-}
-
-
-// before getting any info Authentification first before users , dishes ...
-app.use(auth);
+// function auth (req, res, next) {
+//     if (!req.user) {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       return next(err);
+//     }
+//     else {
+//         next();
+//     }
+// }
+//
+//
+// // before getting any info Authentification first before users , dishes ...
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyPArser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyPArser.json());
@@ -29,9 +30,11 @@ router.post('/signup',( req,res,next) => {
 });
 
 router.post('/login', passport.authenticate('local'),(req, res) => {
+
+  var token = authenticate.getToken({_id:req.user._id}); // user id is sufficant here
   res.statusCode=200;
   res.setHeader('Content-Type', 'application/json');
-  res.json( {success:true, status : 'You are successfuly authenticated '});
+  res.json( {success:true, token :token ,status : 'You are successfuly authenticated '});
 });
 
 router.get('/logout',(req,res,next) => {
